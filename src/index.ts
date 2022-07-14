981ed0338149d10c82b493dfd3e6f8a320026c9f
igolovchuk <igor.golovchuk.work@gmail.com>
2022-07-14 08:45:25 +0300

    dist pathes export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import './dynamo';
-import './etl';
-import './lambda';
-import './s3';
-import './shared';
-import './sns';
+import * as dynamo from './dynamo';
+import * as etl from './etl';
+import * as lambda from './lambda';
+import * as s3 from './s3';
+import * as shared from './shared';
+import * as sns from './sns';
+
+export { dynamo, etl, lambda, s3, shared, sns };
